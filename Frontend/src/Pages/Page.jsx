879c8bd9f7b3ff1bbe62a5e0c05e0bde45757451
Sri-Sharan-Prakash-S <srisharanprakashsuriya@gmail.com>
2024-10-02 21:17:30 +0530

    02-10-2024

--- Frontend/src/Pages/Page.jsx
+++ Frontend/src/Pages/Page.jsx
@@ -1,37 +1,55 @@
 import React, { useEffect, useState } from 'react'
 import { blog_data } from '../assets/gameassets';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import logo from '../../../Frontend/src/assets/gaming-pad.png'
 import './Page.css'
 
 const Page = () => {
     const [data,setData]=useState(null);
-    const location=useLocation();
+    let {id}=useParams();
 
     const fetchData=()=>{
         for(let i=0;i<blog_data.length;i++){
-            if(location.state.id===blog_data[i].id){
+            if(parseInt(id)===blog_data[i].id){
                 setData(blog_data[i]);
                 break;
             }
         }
     }
+    console.log(data);
 
     useEffect(()=>{
         fetchData();
     },[])
-    console.log(data.title);
 
-  return (
-    <div className='bg-gray pt-2 pb-2'>
-        <div className="d-flex justify-content-between align-items-center pr-2 pl-2">
+  return (data?<div>
+    <div className='bg-gray pt-2 pb-5'>
+        <div className="bg-gray d-flex justify-content-between align-items-center pr-2 pl-2">
             <img src={logo} width={40} className='w-20 w-sm-auto' alt="" />
             <button className='d-flex align-items-center gap-2 start'>Get Started</button>
         </div>
         <div className="text-center">
-            <h1>{data.title}</h1>
+            <h1 className='fs-2 fw-bold text-light'>{data.title}</h1>
+            <img className='author' src={data.image} width={60} height={60} alt="" />
+            <p>{data.author}</p>
         </div>
-    </div>
+        </div>
+        <div className='content'>
+            <img className='content-image' src={data.image} width={500} height={400} alt="" />
+            <h1>Introduction:</h1>
+            <p className='fs-5 my-3'>{data.description}</p>
+            <h3 className='fs-4 my-5 fw-bold'>Step 1: Self-Reflection and Goal Setting</h3>
+            <p className='fs-5 my-3'>{data.description}</p>
+            <h3 className='fs-4 my-5 fw-bold'>Step 2: Self-Reflection and Goal Setting</h3>
+            <p className='fs-5 my-3'>{data.description}</p>
+            <h3 className='fs-4 my-5 fw-bold'>Step 3: Self-Reflection and Goal Setting</h3>
+            <p className='fs-5 my-3'>{data.description}</p>
+            <h3 className='fs-4 my-5 fw-bold'>Conclusion</h3>
+
+
+
+        </div>
+        </div>:<></>
   )
 }
 

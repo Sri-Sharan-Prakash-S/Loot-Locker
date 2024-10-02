35ca0aa79a4f488905926ef3f0c0e97f406adfdf
Sri-Sharan-Prakash-S <srisharanprakashsuriya@gmail.com>
2024-10-02 19:42:00 +0530

    02-10-24

--- Frontend/src/Anime/Anime.jsx
+++ Frontend/src/Anime/Anime.jsx
@@ -2,20 +2,25 @@ import React from 'react'
 import './Anime.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import {Link} from 'react-router-dom';
 import arrow from '../../../Frontend/src/assets/arrow-right.png'
 
 const Anime = ({description,title,category,image,id}) => {
   return (
     <div>
     <div style={{backgroundColor:'white'}} className='hover-shadow ml-4 mb-4 mr-4'>
+      <Link to={`/Pages/${id}`} state={{id}}>
       <img src={image} alt='' width={397} height={300} className='image-border'/>
+      </Link>
       <p className='ml-3 mt-3 px-1 d-inline-block bg-dark text-white'>{category}</p>
       <div>
         <h5 className='ml-3 mb-2 fs-4 traking-tight'>{title}</h5>
         <p className='ml-3 mb-3 fs-6'>{description}</p>
         <div className=' ml-3 mb-5 d-inline-flex align-items-center py-2 fw-bold'>
+          <Link to={`/Pages/${id}`} state={{id}}>
           Read more
           <img className='arrow ml-2' src={arrow} alt="" />
+          </Link>
         </div>
       </div>
       </div>
--- Frontend/src/Pages/Page.jsx
+++ Frontend/src/Pages/Page.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { blog_data } from '../assets/gameassets';
+import { useLocation } from 'react-router-dom';
+import logo from '../../../Frontend/src/assets/gaming-pad.png'
+import './Page.css'
 
 const Page = () => {
+    const [data,setData]=useState(null);
+    const location=useLocation();
+
+    const fetchData=()=>{
+        for(let i=0;i<blog_data.length;i++){
+            if(location.state.id===blog_data[i].id){
+                setData(blog_data[i]);
+                break;
+            }
+        }
+    }
+
+    useEffect(()=>{
+        fetchData();
+    },[])
+    console.log(data.title);
+
   return (
-    <div>Page</div>
+    <div className='bg-gray pt-2 pb-2'>
+        <div className="d-flex justify-content-between align-items-center pr-2 pl-2">
+            <img src={logo} width={40} className='w-20 w-sm-auto' alt="" />
+            <button className='d-flex align-items-center gap-2 start'>Get Started</button>
+        </div>
+        <div className="text-center">
+            <h1>{data.title}</h1>
+        </div>
+    </div>
   )
 }
 
